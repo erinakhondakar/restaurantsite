e5e5bf05dacd44de402fa0b3e65fbc4e86aa2ec1
erinakhondakar <khondakarerina@gmail.com>
2026-03-31 22:11:03 -0400

    Added responsive hamburger menu and fixed layout

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function toggleMenu(){
-const nav=document.querySelector(".nav-links");
-nav.classList.toggle("show");
+    const nav=document.querySelector(".nav-links");
+    nav.classList.toggle("show");
 }
 
 let currentSlide=0;
@@ -8,27 +8,21 @@ let currentSlide=0;
 const slides=document.querySelectorAll(".slide");
 
 function showSlide(index){
+    if(index>=slides.length){
+        currentSlide=0;
+    } else if(index<0){
+        currentSlide=slides.length-1;
+    } else{
+        currentSlide=index;
+    }
+    
+    slides.forEach(slide=>slide.classList.remove("active"));
 
-if(index>=slides.length){
-currentSlide=0;
-}
-
-else if(index<0){
-currentSlide=slides.length-1;
-}
-
-else{
-currentSlide=index;
-}
-
-slides.forEach(slide=>slide.classList.remove("active"));
-
-slides[currentSlide].classList.add("active");
-
+    if (slides.length > 0) {
+        slides[currentSlide].classList.add("active");
+    }
 }
 
 function moveSlide(step){
-
-showSlide(currentSlide+step);
-
+    showSlide(currentSlide+step);
 }
